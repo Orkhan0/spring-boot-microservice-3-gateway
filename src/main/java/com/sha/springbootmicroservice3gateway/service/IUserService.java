1815e98bc6fa15bdf9b1289d2bdf5d7d989a3687
Orkhan0 <safarovorkhan211@gmail.com>
2021-07-07 02:10:19 +0400

    initial

---
+++ src/main/java/com/sha/springbootmicroservice3gateway/service/IUserService.java
@@ -0,0 +1,18 @@
+package com.sha.springbootmicroservice3gateway.service;
+
+import com.sha.springbootmicroservice3gateway.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUserService {
+
+
+    User saveUser(User user);
+
+    Optional<User> findByUsername(String username);
+
+    void deleteUser(Long id);
+
+    List<User> findAllUsers();
+}
